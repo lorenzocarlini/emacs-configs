b7a483b9754800e2efe585a726fc8362cd46ffcc
Lan <carlini.lorenzo@protonmail.com>
2025-11-06 12:14:53 +0100

    Added configs and packages for latex editing.

--- init.el
+++ init.el
@@ -6,44 +6,83 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;;disable splash screen and startup message
-(setq inhibit-startup-message t) 
-(setq initial-scratch-message nil)
+;; Disable splash screen and initial scratch message
+(setq inhibit-startup-message t
+      initial-scratch-message nil)
 
-;; Install packages if missing
-(dolist (pkg '(eglot company clang-format))
+;; Ensure packages are installed
+(dolist (pkg '(eglot company clang-format auctex pdf-tools doom-themes))
   (unless (package-installed-p pkg)
     (package-refresh-contents)
     (package-install pkg)))
 
 ;; -------------------------------
-;; Eglot Configuration
+;; AUCTeX / LaTeX Configuration
+;; -------------------------------
+(require 'tex)
+(require 'preview)
+
+;; Enable PDF mode by default
+(setq TeX-PDF-mode t
+      TeX-process-asynchronous t
+      TeX-save-query nil        ;; Don't ask to save buffers
+      TeX-show-compilation nil    ;; Show compilation buffer
+      TeX-command-run-all t     ;; Compile -> view -> bibtex automatically
+      TeX-command-default "LaTeX")
+
+;; Preview settings
+(add-hook 'LaTeX-mode-hook 'LaTeX-preview-setup)
+(setq preview-image-type 'png)
+
+
+
+;; PDF Tools setup
+(use-package pdf-tools
+  :ensure t
+  :config
+  (pdf-tools-install)
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools")))
+  (setq TeX-source-correlate-start-server t)
+  ;; Auto-refresh PDF after compilation
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer))
+
+;; Automatically revert DocView buffers if used
+(add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
+;; Optional: force C-c C-c to compile & refresh without prompts
+(defun my-latex-build-and-view ()
+  "Compile LaTeX and refresh PDF buffer without prompts."
+  (interactive)
+  (save-buffer)
+  (TeX-command "LaTeX" 'TeX-master-file)
+  (TeX-revert-document-buffer))
+(with-eval-after-load 'latex
+  (define-key LaTeX-mode-map (kbd "C-c C-c") 'my-latex-build-and-view))
+
+;; -------------------------------
+;; Eglot (LSP) Configuration
 ;; -------------------------------
 (add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 
-;; Explicitly tell Eglot to use clangd
 (setq eglot-server-programs
       '((c-mode . ("clangd"))
         (c++-mode . ("clangd"))))
-
-;; Make Eglot a bit more responsive
-(setq eglot-send-changes-idle-time 0.5)
-(setq eglot-autoreconnect t)
+(setq eglot-send-changes-idle-time 0.5
+      eglot-autoreconnect t)
 
 ;; -------------------------------
 ;; Company (Completion) Setup
 ;; -------------------------------
 (add-hook 'after-init-hook 'global-company-mode)
 
-
-;; Optional tweaks for smoother experience
-(setq company-idle-delay 0.2        ;; how soon completion pops up
+(setq company-idle-delay 0.2
       company-minimum-prefix-length 1
       company-tooltip-align-annotations t)
 
-;; No company-mode in shell-mode
-(add-hook 'shell-mode-hook (lambda () (global-company-mode 0)))
+;; Disable company in shell-mode
+(add-hook 'shell-mode-hook (lambda () (company-mode -1)))
 
 ;; -------------------------------
 ;; Clang-Format Integration
@@ -51,50 +90,37 @@
 (require 'clang-format)
 
 (defun my-c-cpp-format-buffer ()
-  "Format buffer with clang-format if available."
+  "Format buffer with clang-format if in C/C++ mode."
   (when (derived-mode-p 'c-mode 'c++-mode)
     (clang-format-buffer)))
 
 ;; Auto-format before saving C/C++ files
 (add-hook 'before-save-hook 'my-c-cpp-format-buffer)
 
-;; Optional: bind manual formatting to a key
+;; Optional: manual formatting keybinding
 (with-eval-after-load 'cc-mode
   (define-key c-mode-map  (kbd "C-c f") 'clang-format-buffer)
   (define-key c++-mode-map (kbd "C-c f") 'clang-format-buffer))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; -------------------------------
-;; Doom-Themes Config
+;; Doom-Themes Configuration
 ;; -------------------------------
-
 (use-package doom-themes
   :ensure t
   :custom
-  ;; Global settings (defaults)
-  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
-  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;; for treemacs users
-  (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  (doom-themes-treemacs-theme "doom-atom")
   :config
   (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (nerd-icons must be installed!)
   (doom-themes-neotree-config)
-  ;; or for treemacs users
   (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
+;; -------------------------------
+;; Custom-set Variables / Faces
+;; -------------------------------
+(custom-set-variables
+ '(package-selected-packages nil))
+(custom-set-faces)
