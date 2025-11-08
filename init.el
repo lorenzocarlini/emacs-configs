597313de4129639c3080af71d31f45c3eca161e2
Lan <carlini.lorenzo@protonmail.com>
2025-11-08 13:51:09 +0100

    Cleanup

--- init.el
+++ init.el
@@ -1,45 +1,89 @@
 ;; -------------------------------
-;; Basic Package Setup
+;; Basic Emacs Setup
 ;; -------------------------------
+(setq inhibit-startup-message t
+      initial-scratch-message nil)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(setq inhibit-startup-message t
-      initial-scratch-message nil)
-
-(dolist (pkg '(eglot company clang-format auctex pdf-tools doom-themes))
-  (unless (package-installed-p pkg)
-    (package-refresh-contents)
-    (package-install pkg)))
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 ;; -------------------------------
-;; AUCTeX / LaTeX Configuration
+;; General Utilities
 ;; -------------------------------
-(require 'tex)
-(require 'preview)
+(global-display-line-numbers-mode 1)
+(setq-default indent-tabs-mode nil)
 
-(setq TeX-PDF-mode t
-      TeX-process-asynchronous t
-      TeX-save-query nil
-      TeX-show-compilation nil
-      TeX-command-run-all t
-      TeX-command-default "LaTeX")
+;; -------------------------------
+;; Company
+;; -------------------------------
+(use-package company
+  :hook (after-init . global-company-mode)
+  :custom
+  (company-idle-delay 0.2)
+  (company-minimum-prefix-length 1)
+  (company-tooltip-align-annotations t)
+  :config
+  (add-hook 'shell-mode-hook (lambda () (company-mode -1))))
 
-(add-hook 'LaTeX-mode-hook 'LaTeX-preview-setup)
-(setq preview-image-type 'png)
+;; -------------------------------
+;; Eglot (LSP)
+;; -------------------------------
+(use-package eglot
+  :hook ((c-mode c++-mode python-mode) . eglot-ensure)
+  :custom
+  (eglot-send-changes-idle-time 0.5)
+  (eglot-autoreconnect t)
+  :config
+  (add-to-list 'eglot-server-programs '(c-mode . ("clangd")))
+  (add-to-list 'eglot-server-programs '(c++-mode . ("clangd")))
+  (add-to-list 'eglot-server-programs '(python-mode . ("pylsp"))))
+
+;; -------------------------------
+;; Clang-Format
+;; -------------------------------
+(use-package clang-format
+  :defer t
+  :hook (before-save . my-c-cpp-format-buffer)
+  :config
+  (defun my-c-cpp-format-buffer ()
+    "Format buffer with clang-format if in C/C++ mode."
+    (when (derived-mode-p 'c-mode 'c++-mode)
+      (clang-format-buffer)))
+  :bind (:map c-mode-map ("C-c f" . clang-format-buffer)
+              :map c++-mode-map ("C-c f" . clang-format-buffer)))
+
+;; -------------------------------
+;; AUCTeX / LaTeX
+;; -------------------------------
+(use-package tex
+  :ensure auctex
+  :defer t
+  :hook (LaTeX-mode . LaTeX-preview-setup)
+  :config
+  (setq TeX-PDF-mode t
+        TeX-process-asynchronous t
+        TeX-save-query nil
+        TeX-show-compilation nil
+        TeX-command-default "LaTeX"))
 
 (use-package pdf-tools
-  :ensure t
+  :defer t
   :config
   (pdf-tools-install)
-  (setq TeX-view-program-selection '((output-pdf "PDF Tools")))
-  (setq TeX-source-correlate-start-server t)
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+        TeX-source-correlate-start-server t)
   (add-hook 'TeX-after-compilation-finished-functions
-            #'TeX-revert-document-buffer))
-
-(add-hook 'doc-view-mode-hook 'auto-revert-mode)
+            #'TeX-revert-document-buffer)
+  (add-hook 'doc-view-mode-hook 'auto-revert-mode))
 
 (defun my-latex-build-and-view ()
   "Compile LaTeX and refresh PDF buffer without prompts."
@@ -51,45 +95,11 @@
   (define-key LaTeX-mode-map (kbd "C-c C-c") 'my-latex-build-and-view))
 
 ;; -------------------------------
-;; Eglot Configuration
+;; Python
 ;; -------------------------------
-(add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'c++-mode-hook 'eglot-ensure)
-
-(setq eglot-server-programs
-      '((c-mode . ("clangd"))
-        (c++-mode . ("clangd"))))
-(setq eglot-send-changes-idle-time 0.5
-      eglot-autoreconnect t)
-
-;; -------------------------------
-;; Company Setup
-;; -------------------------------
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0.2
-      company-minimum-prefix-length 1
-      company-tooltip-align-annotations t)
-(add-hook 'shell-mode-hook (lambda () (company-mode -1)))
-
-;; -------------------------------
-;; Clang-Format Integration
-;; -------------------------------
-(require 'clang-format)
-
-(defun my-c-cpp-format-buffer ()
-  "Format buffer with clang-format if in C/C++ mode."
-  (when (derived-mode-p 'c-mode 'c++-mode)
-    (clang-format-buffer)))
-(add-hook 'before-save-hook 'my-c-cpp-format-buffer)
-
-(with-eval-after-load 'cc-mode
-  (define-key c-mode-map  (kbd "C-c f") 'clang-format-buffer)
-  (define-key c++-mode-map (kbd "C-c f") 'clang-format-buffer))
-
-;; -------------------------------
-;; Python Mode - Full Portable Setup
-;; -------------------------------
-
+(use-package pyvenv :defer t)
+(use-package python-black :hook (python-mode . python-black-on-save-mode))
+(use-package flycheck :defer t)
 
 ;; Ensure Emacs uses Python 3 globally
 (let ((python3-bin (or (executable-find "python3")
@@ -99,24 +109,8 @@
     (add-to-list 'exec-path (file-name-directory python3-bin))
     (setenv "PATH" (concat (file-name-directory python3-bin) ":" (getenv "PATH")))))
 
-;; Ensure the Emacs debugger (M-x pdb) uses python3 -m pdb
 (setq gud-pdb-command-name "python3 -m pdb")
-
-;; Set Python indent globally before loading python-black
-(setq python-indent-offset 4
-      indent-tabs-mode nil)
-
-;; Load python-black early (so it sees python-indent-offset)
-(use-package python-black
-  :ensure t)
-
-(use-package pyvenv
-  :ensure t
-  :defer t)
-
-(use-package flycheck
-  :ensure t
-  :defer t)
+(setq python-indent-offset 4)
 
 (defun my-auto-activate-venv ()
   "Activate project .venv if present."
@@ -128,39 +122,29 @@
       (message "Activated virtualenv: %s" venv-dir))))
 
 (defun my-python-setup ()
-  "Setup Python environment for current buffer."
+  "Configure Python environment."
   (my-auto-activate-venv)
   (company-mode 1)
   (eldoc-mode 1)
-  (display-line-numbers-mode 1)
-  ;; Buffer-local font-lock TODOs
-  (font-lock-add-keywords nil
-                          '(("\\<\\(TODO\\|FIXME\\|NOTE\\):" 1 font-lock-warning-face t)))
-  ;; Enable python-black-on-save per buffer
-  (python-black-on-save-mode 1)
-  ;; Keybinding for manual formatting
+  ;; Highlight TODOs/FIXMEs
+  (font-lock-add-keywords nil '(("\\<\\(TODO\\|FIXME\\|NOTE\\):" 1 font-lock-warning-face t)))
+  ;; Keybinding for manual Black formatting
   (define-key python-mode-map (kbd "C-c f") 'python-black-buffer)
-  ;; Flycheck setup
-  (require 'flycheck)
-  (with-eval-after-load 'eglot
-    (add-hook 'eglot-managed-mode-hook (lambda () (flycheck-mode -1))))
+  ;; Disable flycheck in Eglot-managed buffers
+  (add-hook 'eglot-managed-mode-hook (lambda () (flycheck-mode -1)) nil t)
+  ;; Enable flycheck only if Eglot is not active
   (unless (and (fboundp 'eglot-managed-p) (eglot-managed-p))
     (flycheck-mode 1))
-  ;; Eglot LSP
+  ;; Start Eglot if pylsp exists
   (when (executable-find "pylsp")
     (eglot-ensure)))
 
+
 (add-hook 'python-mode-hook 'my-python-setup)
 
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs
-               '(python-mode . ("pylsp"))))
-
-;; -------------------------------
-;; Run Python File in Bottom Buffer
-;; -------------------------------
+;; Run Python file in bottom buffer
 (defun my-run-python-file ()
-  "Save, format with black, and run the current Python file in a small bottom buffer."
+  "Save, format with Black, and run the current Python file in a bottom buffer."
   (interactive)
   (save-buffer)
   (when (eq major-mode 'python-mode)
@@ -179,16 +163,10 @@
           (lambda ()
             (local-set-key (kbd "<f5>") 'my-run-python-file)))
 
-
 ;; -------------------------------
-;; Doom-Themes Configuration
+;; Doom Themes
 ;; -------------------------------
 (use-package doom-themes
-  :ensure t
-  :custom
-  (doom-themes-enable-bold t)
-  (doom-themes-enable-italic t)
-  (doom-themes-treemacs-theme "doom-atom")
   :config
   (load-theme 'doom-one t)
   (doom-themes-visual-bell-config)
@@ -197,20 +175,10 @@
   (doom-themes-org-config))
 
 ;; -------------------------------
-;; Custom-set Variables / Faces
-;; -------------------------------
-(custom-set-variables
- '(package-selected-packages
-   '(all-the-icons doom-modeline yasnippet pyvenv python-black pdf-tools flycheck doom-themes company clang-format auctex)))
-(custom-set-faces)
-
-;; -------------------------------
-;; Doom Modeline - Portable (No Icon Fonts)
+;; Doom Modeline (No Icon Fonts)
 ;; -------------------------------
 (use-package doom-modeline
-  :ensure t
-  :init
-  (doom-modeline-mode 1)
+  :hook (after-init . doom-modeline-mode)
   :custom
   (doom-modeline-icon nil)
   (doom-modeline-major-mode-icon nil)
@@ -222,3 +190,11 @@
   (doom-modeline-bar-width 1)
   (doom-modeline-height 18)
   (doom-modeline-minor-modes nil))
+
+;; -------------------------------
+;; Custom Variables
+;; -------------------------------
+(custom-set-variables
+ '(package-selected-packages
+   '(doom-modeline yasnippet pyvenv python-black pdf-tools flycheck doom-themes company clang-format auctex)))
+(custom-set-faces)
